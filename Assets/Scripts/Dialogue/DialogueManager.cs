12d91292b67b9ce846265eba559cc7c3ef28ff7d
Alvin <chenalvin337@gmail.com>
2024-04-06 20:26:43 -0700

    dialogue scene added and ready to merge

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Ink.Runtime;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private GameObject dialoguePanel;
+
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [SerializeField] private TextAsset dialogue;
+
+    private Story currentStory;
+    private string currentSpeaker;
+
+    private bool dialogueIsPlaying;
+
+    private static DialogueManager instance;
+
+    public static DialogueManager Instance
+    {
+        get
+        {
+            if (instance is null)
+                Debug.Log("DialogueManager is null");
+            return instance;
+        }
+    }
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (instance != null)
+            Debug.LogError("More than one DialogueManager in scene");
+        instance = this;
+        dialogueIsPlaying = false;
+    }
+
+    private void Start()
+    {
+        EnterDialogueMode();
+    }
+
+
+    public void EnterDialogueMode()
+    {
+        currentStory = new Story(dialogue.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+        ContinueStory();
+    }
+
+    private void ExitDialogueMode()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+        currentStory.UnbindExternalFunction("SetSpeaker");
+        currentSpeaker = "";
+    }
+
+    private void SwitchSpeaker(string currentSpeaker)
+    {
+        //do stuff and cry!!!
+        Debug.Log("speaker switched to " + currentSpeaker);
+    }
+
+    public void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            Debug.Log(currentStory.currentTags);
+            if (currentStory.currentTags[0] != currentSpeaker)
+            {
+                currentSpeaker = currentStory.currentTags[0];
+                SwitchSpeaker(currentSpeaker);
+            }
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+}
